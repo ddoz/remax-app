1d2c412dbbc8fb0f9146834f5a0493c32cee9b90
Aulia Ridho Verdikha <auliaridhoverdika@gmail.com>
2021-11-07 22:16:42 +0700

    add bottom navigation

--- lib/screens/home/components/bottom_bar.dart
+++ lib/screens/home/components/bottom_bar.dart
@@ -5,11 +5,39 @@ import 'package:remax_app/screens/maps/map_page.dart';
 import 'package:remax_app/screens/search/search_screen.dart';
 import 'package:remax_app/screens/sign_in/sign_in_page.dart';
 import 'package:remax_app/util/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({
+class BottomNavBar extends StatefulWidget {
+  BottomNavBar({
     Key key,
   }) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+enum LoginStatus { notSignIn, signIn }
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  var value;
+  LoginStatus _loginStatus = LoginStatus.notSignIn;
+
+  getPref() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    setState(() {
+      value = preferences.getInt("value");
+      print(value);
+
+      _loginStatus = value == 1 ? LoginStatus.signIn : LoginStatus.notSignIn;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getPref();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +64,7 @@ class BottomNavBar extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           GestureDetector(
-            onTap: (){
-            },
+            onTap: () {},
             child: Container(
               margin: EdgeInsets.all(10.0),
               child: Column(children: <Widget>[
@@ -53,9 +80,9 @@ class BottomNavBar extends StatelessWidget {
           ),
           Spacer(),
           GestureDetector(
-            onTap: (){
-                  Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext context) => new FavoritePage()));
+            onTap: () {
+              Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (BuildContext context) => new FavoritePage()));
             },
             child: Container(
               margin: EdgeInsets.all(10.0),
@@ -71,8 +98,28 @@ class BottomNavBar extends StatelessWidget {
             ),
           ),
           Spacer(),
+          _loginStatus == LoginStatus.signIn
+              ? GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(new MaterialPageRoute(
+                        builder: (BuildContext context) => SignInPage()));
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    child: Column(children: <Widget>[
+                      SizedBox(
+                          height: 16.0,
+                          child: Icon(Icons.add, color: kPrimaryColor,)),
+                      Text(
+                        'Add', style: TextStyle(fontSize: 13.0),
+                      )
+                    ]),
+                  ),
+                )
+              : Container(),
+          Spacer(),
           GestureDetector(
-            onTap: (){
+            onTap: () {
               Navigator.of(context).push(new MaterialPageRoute(
                   builder: (BuildContext context) => SearchScreen()));
             },
@@ -81,7 +128,10 @@ class BottomNavBar extends StatelessWidget {
               child: Column(children: <Widget>[
                 SizedBox(
                     height: 16.0,
-                    child: SvgPicture.asset("assets/icons/search.svg", color: kPrimaryColor,)),
+                    child: SvgPicture.asset(
+                      "assets/icons/search.svg",
+                      color: kPrimaryColor,
+                    )),
                 Text(
                   'Search',
                   style: TextStyle(fontSize: 13.0),
@@ -90,24 +140,44 @@ class BottomNavBar extends StatelessWidget {
             ),
           ),
           Spacer(),
-          GestureDetector(
-            onTap: (){
-              Navigator.of(context).push(new MaterialPageRoute(
-                  builder: (BuildContext context) => SignInPage()));
-            },
-            child: Container(
-              margin: EdgeInsets.all(10.0),
-              child: Column(children: <Widget>[
-                SizedBox(
-                    height: 16.0,
-                    child: SvgPicture.asset("assets/icons/signin.svg")),
-                Text(
-                  'Sign In',
-                  style: TextStyle(fontSize: 13.0),
+          _loginStatus == LoginStatus.signIn
+              ? GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(new MaterialPageRoute(
+                        builder: (BuildContext context) => SignInPage()));
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    child: Column(children: <Widget>[
+                      SizedBox(
+                          height: 16.0,
+                          child: SvgPicture.asset("assets/icons/person.svg",
+                              color: kPrimaryColor)),
+                      Text(
+                        'Account',
+                        style: TextStyle(fontSize: 13.0),
+                      )
+                    ]),
+                  ),
                 )
-              ]),
-            ),
-          ),
+              : GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(new MaterialPageRoute(
+                        builder: (BuildContext context) => SignInPage()));
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    child: Column(children: <Widget>[
+                      SizedBox(
+                          height: 16.0,
+                          child: SvgPicture.asset("assets/icons/signin.svg")),
+                      Text(
+                        'Sign In',
+                        style: TextStyle(fontSize: 13.0),
+                      )
+                    ]),
+                  ),
+                )
         ],
       ),
     );
